f0d500b1cdf0d282ef9b2bfbc3f50dbd9f6036ca
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-09-11 00:01:50 -0400

    Changed the html IDs so they will line up with the moment time output. added high view pseudocoding to script file.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
 
+// Date and day pulled from moment displayed in header.
 var dateTime = moment().format("dddd, MMMM Do")
 console.log(dateTime);
-
 $("#currentDay").text(dateTime);
+
+// Create variables to tag the needed html elements.
+var currentHour = moment().hours() - 14;
+console.log(currentHour);
+// Set up the text area and button to be saved in local storage with onclick listeners.
+// Connect every hour to local storage independently.
+// using conditional statements, assign the past, present, and future css styles to each text area in relation to the currentHour.
+
+
+
 
 
 });
